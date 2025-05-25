f23436831d01e29fb985d000fd347c203c8659ca
Humberto Dias <hldtux@gmail.com>
2025-05-24 22:22:05 -0300

    assigning field to itself

--- src/animation.cpp
+++ src/animation.cpp
@@ -19,9 +19,9 @@ Animation::Animation(const Animation &a)
    is_combo      = a.is_combo;
    is_continual  = a.is_continual;
    inverted      = a.inverted;
-   x_pos         = x_pos;
-   y_pos         = y_pos;
-   scale         = scale;
+   x_pos         = a.x_pos;
+   y_pos         = a.y_pos;
+   scale         = a.scale;
 
    for(int i = 0; i < a.sprites.size(); i++)
    {
