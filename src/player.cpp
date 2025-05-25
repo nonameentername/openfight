041c89564c7854e5b82ee51e3ce2c8fa86db3b0d
Humberto Dias <hldtux@gmail.com>
2025-05-24 21:47:50 -0300

    warning: assigning field to itself

--- src/player.cpp
+++ src/player.cpp
@@ -27,8 +27,8 @@ Player::Player(const Player &p)
 
    type     = p.type;
    inverted = p.inverted;
-   is_hurt  = is_hurt;
-   index    = index;
+   is_hurt  = p.is_hurt;
+   index    = p.index;
 
    current_state  = p.current_state;
    previous_state = p.previous_state;
