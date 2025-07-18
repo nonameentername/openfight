4fa6bf874a83045324e2bd7c842974ca1fa4ce4d
Humberto <hldtux@gmail.com>
2025-07-18 17:55:30 -0300

    comments

--- run-linux.sh
+++ run-linux.sh
@@ -6,8 +6,10 @@ trap 'echo -e "\n❌ Error on line $LINENO. Pausing..."; read -p "Press Enter to
 # Move to the script's directory
 cd "$(dirname "$(realpath "$0")")"
 
+# install the required packages
 sudo apt install -y build-essential libxml2-dev libsdl2-dev libsdl2-image-dev libglu1-mesa-dev libglew-dev
 
+# Build
 cmake -Bbuild
 cmake --build build
 
